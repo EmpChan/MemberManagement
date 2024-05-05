ae9589103f023d1baaeed75be52b096f4cecf092
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-05-06 03:06:25 +0900

    Change some dirs and settings
    css불러오기랑 html 올리는거 다해뒀슴~

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from main.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', test, name = "home"),
 ]
