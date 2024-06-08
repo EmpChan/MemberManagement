b7fed71543cd6bba953e1b07fd9c299ef871aadc
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-06-08 22:46:48 +0900

    FIX login

--- login/urls.py
+++ login/urls.py
@@ -7,5 +7,6 @@ app_name = "login"
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='login/login.html'), name = "login"),
+    path('logout',auth_views.LogoutView.as_view(), name="logout"),
     path('register', register, name = "register"),
 ]
