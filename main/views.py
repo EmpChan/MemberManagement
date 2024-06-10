9de7aef24b60b8a056656e144046694ba10b0471
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-06-10 22:52:52 +0900

    모델 수정 삭제기능 구현

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Member
@@ -40,20 +40,46 @@ def linkList(request):
 def linkLevelList(request):
     if not request.user.is_authenticated:
         return redirect("login:login")
-    return render(request, "linkLevel/linkLevelList.html")
+    groupList = Member.objects.filter(gender="GROUP")
+    if not request.method == 'POST':
+        return render(request, "linkLevel/linkLevelList.html",{"groupList" : groupList})
+    try:
+        newMember = Member()
+        newMember.name = request.POST['groupName']
+        newMember.gender = "GROUP"
+        newMember.studentID = "GROUP"
+        newMember.department = "GROUP"
+        newMember.type = 1
+        newMember.save()
+    except:
+        print(newMember)
+    return redirect("mainpages:linkLevelList")
 
-def objectView(request):
+def objectView(request,id):
     if not request.user.is_authenticated:
         return redirect("login:login")
-    memberList = Member.objects.all()
-    attribute = {}
-    attribute['members'] = memberList
-    return render(request, "object/objectView.html")
+    objec = get_object_or_404(Member, id=id)
+    attribute = {'object':objec}
+    if not request.method=="POST":
+        print("흠")
+        return render(request, "object/objectView.html", attribute)
+    objec.name= request.POST['username']
+    objec.department = request.POST['department']
+    objec.studentID = request.POST['studentID']
+    objec.gender = request.POST['gender']
+    if request.POST['notSchool'] == 'Yes':
+        objec.notSchool = True
+    else:
+        objec.notSchool = False
+    print("캬")
+    objec.save()
+    return redirect("mainpages:objectList")
+
 
 def objectList(request):
     if not request.user.is_authenticated:
         return redirect("login:login")
-    memberList = Member.objects.all()
+    memberList = Member.objects.exclude(gender__icontains="GROUP")
     paginator = Paginator(memberList, 10)  # 페이지당 10개의 아이템을 보여주도록 설정
 
     page = request.GET.get('page', 1)  # URL의 'page' 파라미터 값을 가져오거나, 없으면 1로 설정
@@ -66,8 +92,8 @@ def objectList(request):
     except EmptyPage:
         # 페이지 범위를 벗어난 경우, 마지막 페이지를 보여줌
         members = paginator.page(paginator.num_pages)
-
-    attribute = {'members': members}
+    attribute ={}
+    attribute['members']= members
     return render(request, "object/objectList.html", attribute)
 
 def objectRegister(request):
@@ -88,6 +114,32 @@ def objectRegister(request):
     print(newMember)
 
     return render(request, "object/objectRegister.html")
+
+def deleteGroup(request):
+    if not request.user.is_authenticated:
+        return redirect("login:login")
+    if not request.method=="POST":
+        return redirect("mainpages:linkLevelList")
+    memberList = list(Member.objects.filter(gender="GROUP"))
+    checkBox = request.POST.getlist('selectBox')
+    print(memberList)
+    for i in checkBox:
+        memberList[int(i)].delete()
+
+    return redirect("mainpages:linkLevelList")
+
+def deleteMember(request):
+    if not request.user.is_authenticated:
+        return redirect("login:login")
+    if not request.method=="POST":
+        return redirect("mainpages:objectList")
+    memberList = Member.objects.exclude(gender__icontains="GROUP")
+    checkBox = request.POST.getlist('selectBox')
+    print(memberList)
+    for i in checkBox:
+        memberList[int(i)].delete()
+
+    return redirect("mainpages:objectList")
 
 def setupMain(request):
     if not request.user.is_authenticated:
